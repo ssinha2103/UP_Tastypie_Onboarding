43aa9bca1bd392a73f6b7758315c3739f2d84ea6
Sudarshan Sinha <s.sinha2103@outlook.com>
2022-11-04 16:12:09 +0530

    add custom authentication to the project

--- UP_Tastypie_Onboarding/api/resources.py
+++ UP_Tastypie_Onboarding/api/resources.py
@@ -2,18 +2,23 @@ from point_of_sale.models import *
 from tastypie import fields
 from django.contrib.auth.models import User
 from tastypie.resources import ModelResource
-from tastypie.authentication import Authentication
+from tastypie.authentication import BasicAuthentication, Authentication
 from tastypie.authorization import Authorization
+from point_of_sale.permissions import *
+from django.contrib.auth import authenticate, login, logout
+from tastypie.http import HttpUnauthorized, HttpForbidden
+from django.conf.urls import url
 
 
 class UserResource(ModelResource):
     """Resource for User model (This model is DJANGO user model)."""
+
     class Meta:
         queryset = User.objects.all()
         resource_name = 'users'
         fields = ['username', 'email', 'password']
-        authentication = Authentication()
-        authorization = Authorization()
+        authentication = PassAuthentication()
+        authorization = PassAuthorization()
         include_resource_uri = False
 
 
@@ -24,9 +29,64 @@ class ProfileResource(ModelResource):
     class Meta:
         queryset = Profile.objects.all()
         resource_name = "profiles"
-        authentication = Authentication()
-        authorization = Authorization()
+        authentication = PassAuthentication()
+        authorization = PassAuthorization()
         include_resource_uri = False
+
+    def obj_create(self, bundle, **kwargs):
+        """Overrides the obj_create method of ModelResource"""
+        user_data = bundle.data.pop("user")
+        user = User.objects.create(
+            username=user_data["username"],
+            email=user_data["email"],
+        )
+        user.set_password(user_data["password"])
+        user.save()
+        profile = Profile.objects.create(user=user, role=bundle.data["role"], name=bundle.data["name"])
+        bundle.obj = self._meta.object_class()
+        return profile
+
+    def prepend_urls(self):
+        return [
+            url(r"^user/login/$", self.wrap_view('login'), name="api_login"),
+            url(r"^user/logout/$", self.wrap_view('logout'), name='api_logout'),
+        ]
+
+    def login(self, request, **kwargs):
+        self.method_check(request, allowed=['post'])
+
+        data = self.deserialize(request, request.body, format=request.META.get('CONTENT_TYPE', 'application/json'))
+
+        username = data.get('username', '')
+        password = data.get('password', '')
+        print(username)
+        print(password)
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return self.create_response(request, {
+                    'success': True
+                })
+            else:
+                return self.create_response(request, {
+                    'success': False,
+                    'reason': 'disabled',
+                }, HttpForbidden)
+        else:
+            return self.create_response(request, {
+                'success': False,
+                'reason': 'incorrect',
+            }, HttpUnauthorized)
+
+    def logout(self, request, **kwargs):
+        self.method_check(request, allowed=['get'])
+        if request.user and request.user.is_authenticated:
+            logout(request)
+            return self.create_response(request, {'success': True})
+        else:
+            return self.create_response(request, {'success': False}, HttpUnauthorized)
 
 
 class StoreResource(ModelResource):
@@ -34,11 +94,15 @@ class StoreResource(ModelResource):
     merchant = fields.ToOneField('api.resources.ProfileResource', 'merchant', related_name='store_resource', full=True)
 
     class Meta:
-        authentication = Authentication()
-        authorization = Authorization()
+        authentication = BasicAuthentication()
+        authorization = MerchantStoreAuthorization()
         queryset = Store.objects.all()
         resource_name = 'stores'
         include_resource_uri = False
+
+    def get_object_list(self, request):
+        """Overrides the get_object_list method of ModelResource"""
+        return super(StoreResource, self).get_object_list(request).filter(merchant__user=request.user)
 
 
 class ItemResource(ModelResource):
@@ -46,8 +110,8 @@ class ItemResource(ModelResource):
     stores = fields.ToManyField('api.resources.StoreResource', 'stores', related_name='item_resource', full=True)
 
     class Meta:
-        authentication = Authentication()
-        authorization = Authorization()
+        authentication = BasicAuthentication()
+        authorization = MerchantItemAuthorization()
         queryset = Item.objects.all()
         resource_name = 'items'
         include_resource_uri = False
---
+++ UP_Tastypie_Onboarding/point_of_sale/permissions.py
@@ -0,0 +1,52 @@
+from abc import ABC
+
+from django.http import HttpResponse
+from tastypie.exceptions import Unauthorized
+
+from point_of_sale.models import *
+from tastypie.authorization import Authorization
+from tastypie.authentication import Authentication
+
+
+class MerchantStoreAuthorization(Authorization, ABC):
+    """Custom Authorization model for generating Merchant Authorization"""
+
+    def read_list(self, object_list, bundle):
+        """Overriding read_list method to filter out object_list based on user"""
+        profile = Profile.objects.get(user=bundle.request.user)
+        if profile.role == 1:
+            return object_list.filter(merchant__user=bundle.request.user)
+        else:
+            # raise Unauthorized("No Access")
+            return HttpResponse('Unauthorized', status=401)
+
+    def create_detail(self, object_list, bundle):
+        """Overriding create_detail method to provide write access of stores to only merchants"""
+        profile = Profile.objects.get(user=bundle.request.user)
+        if profile.role == 1:
+            return True
+        else:
+            # raise Unauthorized("No Access")
+            return False
+
+
+class MerchantItemAuthorization(Authorization, ABC):
+    def read_list(self, object_list, bundle):
+        """Overriding read_list method to filter out object_list based on profile"""
+        profile = Profile.objects.get(user=bundle.request.user)
+        if profile.role == 1:
+            return object_list.filter(stores__merchant=profile)
+        else:
+            raise Unauthorized("No Access")
+
+
+class PassAuthentication(Authentication):
+    """Dummy Authentication class which allows some services to all users (logged-in/logged-out)"""
+    def is_authenticated(self, request, **kwargs):
+        return True
+
+
+class PassAuthorization(Authorization, ABC):
+    """Dummy Authorization class which allows some services to all users (logged-in/logged-out)"""
+    def read_list(self, object_list, bundle):
+        return object_list.filter()
